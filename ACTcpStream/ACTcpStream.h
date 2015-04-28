279b72841052099c9e6d43eccda8db1c89f61865
Ralph-Gordon Paul <gordon@Gordon-MBP.local>
2015-04-28 23:55:01 +0200

    Initial Commit

---
+++ ACTcpStream/ACTcpStream.h
@@ -0,0 +1,13 @@
+//
+//  ACTcpStream.h
+//  ACTcpStream
+//
+//  Created by Ralph-Gordon Paul on 28.04.15.
+//  Copyright (c) 2015 appcom interactive GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACTcpStream : NSObject
+
+@end
---
+++ ACTcpStream/ACTcpStream.m
@@ -0,0 +1,13 @@
+//
+//  ACTcpStream.m
+//  ACTcpStream
+//
+//  Created by Ralph-Gordon Paul on 28.04.15.
+//  Copyright (c) 2015 appcom interactive GmbH. All rights reserved.
+//
+
+#import "ACTcpStream.h"
+
+@implementation ACTcpStream
+
+@end
